9ac13466b9cadf6e132f02b57f2cf96073085e07
jaca <vitouejj@gmail.com>
2021-04-17 00:02:37 +0200

    refacot generate_link_library

--- generate_link_library.py
+++ generate_link_library.py
@@ -5,11 +5,13 @@ from typing import Any
 from dataclasses import dataclass
 from jinja2 import Environment, PackageLoader
 
+
 @dataclass
 class Link():
     name: str
     url: str
     has_children: bool = False
+
 
 @dataclass
 class Folder():
@@ -17,20 +19,6 @@ class Folder():
     children: Any
     has_children: bool = True
 
-def main():
-    parser = argparse.ArgumentParser(description='Transform a bookmark file into a simple webpage')
-    parser.add_argument('path', type=str, help='path to the Bookmark file')
-    parser.add_argument('--output', type=str, default='bookmarks.html')
-    argv = parser.parse_args()
-    with open(argv.path) as f, open(argv.output, 'w') as page_f:
-        env = Environment(
-            loader=PackageLoader('__main__', './templates'),
-            trim_blocks=True,
-            lstrip_blocks=True)
-        raw_bookmarks = json.load(f)['roots'] ['bookmark_bar']['children']
-        bookmarks = [to_bookmark(element) for element in raw_bookmarks]
-        template = env.get_template('link_library.jinja.html')
-        page_f.write(template.render(bookmarks=bookmarks))
 
 def to_bookmark(json):
     if json['type'] == 'folder':
@@ -41,6 +29,28 @@ def to_bookmark(json):
     else:
         raise ValueError(f"Cannot create a bookmark of type {json['type']}")
 
+
+def generate_link_library(bookmarks_path, output_path):
+    with open(path) as f, open(output_path, 'w') as page_f:
+        env = Environment(
+            loader=PackageLoader(__name__, './templates'),
+            trim_blocks=True,
+            lstrip_blocks=True)
+
+        raw_bookmarks = json.load(f)['roots']['bookmark_bar']['children']
+        bookmarks = [to_bookmark(element) for element in raw_bookmarks]
+        template = env.get_template('link_library.jinja.html')
+        page_f.write(template.render(bookmarks=bookmarks))
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='Transform a bookmark file into a simple webpage')
+    parser.add_argument('path', type=str, help='path to the Bookmark file')
+    parser.add_argument('--output', type=str, default='bookmarks.html')
+    argv = parser.parse_args()
+    generate_link_library(argv.path, argv.output)
+
+
 if __name__ == '__main__':
     main()
-
